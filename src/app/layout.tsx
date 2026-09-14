184838bc6828ce31474994d127fc42fe98e904d0
kusumarumy <wahyucahyaningrumsuji@gmail.com>
2026-09-14 13:15:42 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
-
+import { Toaster } from 'react-hot-toast';
 import './globals.css';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
