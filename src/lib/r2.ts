17e6d220a4aa91bcedfa0afa31485027860b72fb
kusumarumy <wahyucahyaningrumsuji@gmail.com>
2026-09-16 10:37:29 +0700

    Update r2.ts

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -29,8 +29,10 @@ function s3(): S3Client {
 console.log("R2 CONFIG CHECK", {
   account: process.env.BOJO_R2_ACCOUNT_ID,
   bucket: process.env.BOJO_R2_BUCKET,
-  accessKey: process.env.BOJO_R2_ACCESS_KEY_ID?.slice(0, 6),
-  secretLength: process.env.BOJO_R2_SECRET_ACCESS_KEY?.length,
+  configured: !!(
+    process.env.BOJO_R2_ACCESS_KEY_ID &&
+    process.env.BOJO_R2_SECRET_ACCESS_KEY
+  ),
 });
 
 _s3 = new S3Client({
