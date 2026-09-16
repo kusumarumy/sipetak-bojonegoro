4e82e8754b2b67202eed3ab967fe78707940c009
kusumarumy <wahyucahyaningrumsuji@gmail.com>
2026-09-16 15:56:55 +0700

    Update layers.ts

--- src/components/layers.ts
+++ src/components/layers.ts
@@ -18,7 +18,7 @@ export const LAYERS: DefLayer[] = [
   nama: 'Trase rencana',
   sumber: 'traseg',
   tipe: 'line',
-  warna: '#D32F2F',
+  warna: '#00E5FF',
   garis: true,
   lebar: 4,
   opasitas: 1,
