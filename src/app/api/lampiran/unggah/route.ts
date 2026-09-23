fca6f59f3476b4a016350ce5e29d7806bc685e85
kusumarumy <wahyucahyaningrumsuji@gmail.com>
2026-09-24 06:44:49 +0700

    Update route.ts

--- src/app/api/lampiran/unggah/route.ts
+++ src/app/api/lampiran/unggah/route.ts
@@ -21,7 +21,6 @@ export async function POST(req: NextRequest) {
 
     const file = form.get("file") as File | null;
 
-    // SEKARANG PAKAI FID
     const fidText = form.get("fid") as string | null;
 
     const kategori = form.get("kategori") as string | null;
