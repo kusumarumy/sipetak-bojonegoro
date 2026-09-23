33fc57b307a35a6155c54175fdabe2c9de45a822
kusumarumy <wahyucahyaningrumsuji@gmail.com>
2026-09-23 13:59:58 +0700

    Update KartuBidang.tsx

--- src/components/KartuBidang.tsx
+++ src/components/KartuBidang.tsx
@@ -504,7 +504,7 @@ export default function KartuBidang({
               "application/json",
           },
           body: JSON.stringify({
-            ke: target,
+            status: target,
           }),
         }
       );
